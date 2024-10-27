26b6c6fb1a391426a660c60bc68d7227150eb1b7
John D Simpson <jsimpso9@my.westga.edu>
2024-10-27 11:14:19 -0400

    Buttons and main display added onto form

--- JohnSimpsonA7/CarForm.Designer.cs
+++ JohnSimpsonA7/CarForm.Designer.cs
@@ -28,18 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
+            CarDisplay = new ListBox();
+            FileButton = new Button();
+            SortMake = new Button();
+            SortMakeAndPrice = new Button();
             SuspendLayout();
+            // 
+            // CarDisplay
+            // 
+            CarDisplay.FormattingEnabled = true;
+            CarDisplay.ItemHeight = 15;
+            CarDisplay.Location = new Point(12, 12);
+            CarDisplay.Name = "CarDisplay";
+            CarDisplay.Size = new Size(776, 259);
+            CarDisplay.TabIndex = 0;
+            // 
+            // FileButton
+            // 
+            FileButton.Location = new Point(80, 333);
+            FileButton.Name = "FileButton";
+            FileButton.Size = new Size(98, 23);
+            FileButton.TabIndex = 1;
+            FileButton.Text = "Open New File?";
+            FileButton.UseVisualStyleBackColor = true;
+            // 
+            // SortMake
+            // 
+            SortMake.Location = new Point(278, 333);
+            SortMake.Name = "SortMake";
+            SortMake.Size = new Size(86, 23);
+            SortMake.TabIndex = 2;
+            SortMake.Text = "Sort By Make";
+            SortMake.UseVisualStyleBackColor = true;
+            // 
+            // SortMakeAndPrice
+            // 
+            SortMakeAndPrice.Location = new Point(477, 333);
+            SortMakeAndPrice.Name = "SortMakeAndPrice";
+            SortMakeAndPrice.Size = new Size(87, 38);
+            SortMakeAndPrice.TabIndex = 3;
+            SortMakeAndPrice.Text = "Sort By Make and Price";
+            SortMakeAndPrice.UseVisualStyleBackColor = true;
             // 
             // CarForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(SortMakeAndPrice);
+            Controls.Add(SortMake);
+            Controls.Add(FileButton);
+            Controls.Add(CarDisplay);
             Name = "CarForm";
             Text = "John Simpson Assignment 7";
             ResumeLayout(false);
         }
 
         #endregion
+
+        private ListBox CarDisplay;
+        private Button FileButton;
+        private Button SortMake;
+        private Button SortMakeAndPrice;
     }
 }
